489a3336fa18495067b7ccdcd93cc1a35a3add33
Øivind Heggland <oivind@kdrstavanger.no>
2018-06-13 16:19:12 +0200

    First setup og testing

---
+++ KDRGoldAPI/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using KDRGoldAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KDRGoldAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public TestController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.Users.Count() == 0)
+            {
+                _context.Users.Add(new User { UserName = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<User> GetById(long id)
+        {
+            var item = _context.Users.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
---
+++ KDRGoldAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KDRGoldAPI.Models
+{
+    public class User
+    {
+        public String UserGuid { get; set; }
+        public String UserName { get; set; }
+        public String MsterKey { get; set; }
+        public bool Active { get; set; }
+        public String AppId { get; set; }
+        public int UserId { get; set; }
+        public String Email { get; set; }
+    }
+}
